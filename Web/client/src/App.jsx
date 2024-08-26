283cdb83235ef8fce7af6b06c39a6fce96cde6db
Rajsingh365 <sraj08188@gmail.com>
2024-08-26 19:04:33 +0530

    Bring Routing in App.jsx

--- Web/client/src/App.jsx
+++ Web/client/src/App.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { FeedbackForm, ProgressReportList, SessionList, TherapyPlanList } from "./pages/Supervisor";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <main>
-        <Outlet />
-      </main>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/supervisor/clinical-rating" element={<FeedbackForm />} />
+        <Route path="/supervisor/progress-report-list" element={<ProgressReportList />} />  
+        <Route path="/supervisor/sessions-list" element={<SessionList />} />
+        <Route path="/supervisor/therapy-plan-list" element={<TherapyPlanList />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
--- Web/client/src/main.jsx
+++ Web/client/src/main.jsx
@@ -3,26 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { FeedbackForm, ProgressReportList, SessionList, TherapyPlanList } from "./pages/Supervisor";
-import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "supervisor/clinical-rating", element: <FeedbackForm /> },
-      { path: "supervisor/progress-report-list", element: <ProgressReportList /> },
-      { path: "supervisor/sessions-list", element: <SessionList /> },
-      { path: "supervisor/therapy-plan-list", element: <TherapyPlanList /> },
-      { path: "admin/dashboard", element: <AdminDashboard /> },
-    ],
-  },
-]);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </StrictMode>
 );
