3a3045981e853dab5e9cc7c0e85076b80adffe5c
Taha <tahaalotwala@gmail.com>
2024-08-28 19:26:10 +0530

    Fixing major merge errors

--- Web/client/src/App.jsx
+++ Web/client/src/App.jsx
@@ -1,26 +1,90 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { FeedbackForm, ProgressReportList, SessionList, TherapyPlanList } from "./pages/Supervisor";
+import { MainLayout } from "./pages/Layouts/MainLayout.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ClinicalRatings, ProgressReportList, SessionList, TherapyPlanList, WorkspaceLayout } from "./pages/Supervisor";
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import ProgressReport from "./pages/Therapist/ProgressReport";
+import TherapistProgressReportList from "./pages/Therapist/ProgressReportList";
+import SessionForm from "./pages/Therapist/SessionForm.jsx";
+import SessionFormList from "./pages/Therapist/SessionFormList.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/admin",
+        children: [
+          {
+            path: "dashboard",
+            element: <AdminDashboard />,
+          },
+          {
+            path: "workspace",
+            element: <h1>Admin Workspace</h1>,
+          },
+        ],
+      },
+      {
+        path: "/supervisor",
+        children: [
+          {
+            path: "dashboard",
+            element: <h1>Supervisor Dashboard</h1>,
+          },
+          {
+            path: "workspace",
+            element: <WorkspaceLayout />,
+            children: [
+              {
+                path: "progress-report",
+                element: <ProgressReportList />,
+              },
+              {
+                path: "sessions",
+                element: <SessionList />,
+              },
+              {
+                path: "therapy-plans",
+                element: <TherapyPlanList />,
+              },
+              {
+                path: "clinical-rating",
+                element: <ClinicalRatings />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "/therapist",
+        children: [
+          {
+            path: "progress-report",
+            element: <ProgressReport />,
+          },
+          {
+            path: "progress-report-list",
+            element: <TherapistProgressReportList />,
+          },
+          {
+            path: "session-list",
+            element: <SessionFormList />,
+          },
+          {
+            path: "session-form/:patientId",
+            element: <SessionForm />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<h1>Hello</h1>} />
-        <Route path="/supervisor/clinical-rating" element={<FeedbackForm />} />
-        <Route path="/supervisor/progress-report-list" element={<ProgressReportList />} />  
-        <Route path="/supervisor/sessions-list" element={<SessionList />} />
-        <Route path="/supervisor/therapy-plan-list" element={<TherapyPlanList />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
