eab68a87fe5b6c8678f538071951c456afaf57ad
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2024-08-28 13:48:28 +0530

    merged raj and test

--- Web/client/src/pages/Supervisor/index.js
+++ Web/client/src/pages/Supervisor/index.js
@@ -4,4 +4,4 @@ import { SessionList } from "./SessionList";
 import { TherapyPlanList } from "./TherapyPlanList";
 import SupervisorDashboard from '../../Dashboards/Supervisor/SupervisorDashboard'
 import { NotificationSupervisor } from "./NotificationSupervisor";
-export {SupervisorDashboard, FeedbackForm, ProgressReportList, SessionList, TherapyPlanList, NotificationSupervisor };
+export {SupervisorDashboard, ProgressReportList, SessionList, TherapyPlanList, NotificationSupervisor };
